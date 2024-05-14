41fa9d0c3b879fe023c7eaff2796e5df54807cb8
Jorge Suazo <jsuazof@gmail.com>
2024-05-13 20:43:28 -0400

    update files

--- alke_app/src/main/java/alkewallet/Main.java
+++ alke_app/src/main/java/alkewallet/Main.java
@@ -5,6 +5,6 @@ import alkewallet.model.Cliente;
 public class Main {
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente("Jorge", "Suazo", "15689777-2", "556677");
-        Cliente cliente2 = 
+        Cliente cliente2 = new Cliente("Ruperto", "Mardones", "22536789-1", "777777");
     }
 }
