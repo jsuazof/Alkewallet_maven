a57b5f2a2ec85aea6b2d577e0b0f91d7981cc6a1
Jorge Suazo <jsuazof@gmail.com>
2024-05-13 22:02:32 -0400

    update files

--- alke_app/src/main/java/alkewallet/Main.java
+++ alke_app/src/main/java/alkewallet/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import alkewallet.model.Cliente;
 import alkewallet.model.Cuenta;
+import alkewallet.model.Menu;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +13,7 @@ public class Main {
         Cliente cliente2 = new Cliente("Ruperto", "Mardones", "22536789-1", "777777");
         Cuenta cuenta1 = new Cuenta(123456);
         Cuenta cuenta2 = new Cuenta(654321);
-        
+
         List<Cliente> clientes = new LinkedList<>();
         clientes.add(cliente1);
         clientes.add(cliente2);
@@ -20,5 +21,10 @@ public class Main {
         cliente1.getCuentas().add(cuenta1);
         cliente1.getCuentas().add(cuenta2);
 
+        
+        Menu menu = new Menu();
+        menu.dibujaMenu();
+        menu.dibujaMenuConversor();
     }
+
 }
--- alke_app/src/main/java/alkewallet/service/ServiceCuenta.java
+++ alke_app/src/main/java/alkewallet/service/ServiceCuenta.java
@@ -1,5 +1,20 @@
 package alkewallet.service;
 
+import alkewallet.model.Cuenta;
+
 public class ServiceCuenta {
 
+    public boolean depositar(Cuenta cuenta, double monto){
+        cuenta.setSaldo(cuenta.getSaldo() + monto);
+        return true;
+    
+}
+    public boolean retiro(Cuenta cuenta, double monto){
+        if(cuenta.getSaldo() < monto){
+            System.out.println("Fondos insuficientes");
+            return false;
+        }
+        cuenta.setSaldo(cuenta.getSaldo() - monto);
+        return true;
+    }
 }
