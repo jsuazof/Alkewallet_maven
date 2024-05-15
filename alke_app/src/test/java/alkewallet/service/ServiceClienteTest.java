fa6c76765af1d3d0193a3613e3f25664562a1743
Jorge Suazo <jsuazof@gmail.com>
2024-05-15 10:00:14 -0400

    files updates

---
+++ alke_app/src/test/java/alkewallet/service/ServiceClienteTest.java
@@ -0,0 +1,25 @@
+package alkewallet.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ServiceClienteTest {
+    @Test
+    void testActualizarCliente() {
+
+    }
+
+    @Test
+    void testCrearCliente() {
+
+    }
+
+    @Test
+    void testEliminarCliente() {
+
+    }
+
+    @Test
+    void testLeerClientes() {
+
+    }
+}
---
+++ alke_app/src/test/java/alkewallet/service/ServiceCuentaTest.java
@@ -0,0 +1,15 @@
+package alkewallet.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ServiceCuentaTest {
+    @Test
+    void testDepositar() {
+
+    }
+
+    @Test
+    void testRetiro() {
+
+    }
+}
