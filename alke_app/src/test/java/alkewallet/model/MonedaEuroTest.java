37f5e7e3e8f45f1de08c0b692b811f3b22ceac3b
Jorge Suazo <jsuazof@gmail.com>
2024-05-26 11:34:17 -0400

    update files

--- alke_app/src/test/java/alkewallet/model/MonedaEuroTest.java
+++ alke_app/src/test/java/alkewallet/model/MonedaEuroTest.java
@@ -7,53 +7,53 @@ import org.junit.Test;
 public class MonedaEuroTest {
     @Test
     public void testGetNombre() {
-        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", 1.0);
+        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", (int) 1.0);
         assertEquals("MonedaEuro", euro.getNombre());
     }
 
     @Test
     public void testGetValor() {
-        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", 1.0);
+        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", (int) 1.0);
         assertEquals(1.0, euro.getValor(), 0.001);
     }
 
     @Test
     public void testGetPluralName() {
-        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", 1.0);
+        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", (int) 1.0);
         assertEquals("Euros", euro.getPluralName());
     }
 
     @Test
     public void testGetSimbol() {
-        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", 1.0);
+        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", (int) 1.0);
         assertEquals("€", euro.getSimbolo());
     }
 
     @Test
     public void testSetNombre() {
-        MonedaEuro monedaEuro = new MonedaEuro("MonedaEuro", "€", "Euros", 1.0 );
+        MonedaEuro monedaEuro = new MonedaEuro("MonedaEuro", "€", "Euros", (int) 1.0 );
         monedaEuro.setNombre("MonedaEuro");
         assertEquals("MonedaEuro", monedaEuro.getNombre());
     }
 
     @Test
     public void testSetValor() {
-        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", 1.0 );
+        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", (int) 1.0 );
         euro.setValor(1.0);
         assertEquals(1.0, euro.getValor(), 0.001);
     }
 
     @Test
     public void testSetPluralName() {
-        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", 1.0 );
+        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", (int) 1.0 );
         euro.setPluralName("Euros");
         assertEquals("Euros", euro.getPluralName());
     }
 
     @Test
     public void testSetSimbol() {
-        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", 1.0 );
-        euro.setSimbol("€");
+        MonedaEuro euro = new MonedaEuro("MonedaEuro", "€", "Euros", (int) 1.0 );
+        euro.setSimbolo("€");
         assertEquals("€", euro.getSimbolo());
     }
 }
